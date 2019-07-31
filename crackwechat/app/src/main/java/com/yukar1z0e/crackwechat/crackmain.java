02f9cd825f7a40c71a53fba29f2473cd1d577e45
yukar1z0e <yakumozoe@outlook.com>
2019-07-31 19:22:17 +0800

    "..."
    Signed-off-by: yukar1z0e <yakumozoe@outlook.com>

--- crackwechat/app/src/main/java/com/yukar1z0e/crackwechat/crackmain.java
+++ crackwechat/app/src/main/java/com/yukar1z0e/crackwechat/crackmain.java
@@ -22,7 +22,7 @@ import static de.robv.android.xposed.XposedHelpers.findAndHookConstructor;
 
 public class crackmain implements IXposedHookLoadPackage{
     @Override
-    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam)throws Throwable{
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam){
         Log.d("Begin","Test Xposed--->"+lpparam.packageName);
         if (lpparam.packageName.contains("com.tencent.mm")){
             Log.d("Begin","Xposed Hooked--->"+lpparam.packageName);
