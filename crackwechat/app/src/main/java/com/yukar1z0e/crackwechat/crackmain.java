6c6c40f42ee31be4603d506719c825d0aa1d4473
yukar1z0e <yakumozoe@outlook.com>
2019-08-01 15:09:27 +0800

    "some bug fixed"
    Signed-off-by: yukar1z0e <yakumozoe@outlook.com>

--- crackwechat/app/src/main/java/com/yukar1z0e/crackwechat/crackmain.java
+++ crackwechat/app/src/main/java/com/yukar1z0e/crackwechat/crackmain.java
@@ -31,13 +31,25 @@ public class crackmain implements IXposedHookLoadPackage{
 
             //勾取类
             final Class<?> FTSAddFriendUIClass=lpparam.classLoader.loadClass("com.tencent.mm.plugin.fts.ui.FTSAddFriendUI");
+            final Class<?> FTSAddFriendUI$5Class=lpparam.classLoader.loadClass("com.tencent.mm.plugin.fts.ui.FTSAddFriendUI$5");
+            final Class<?> mClass=lpparam.classLoader.loadClass("com.tencent.mm.ah.m");
 
-            //Hook FTSAssFriendUI.Mf
+
+            //Hook FTSAddFriendUI.Mf
             findAndHookMethod(FTSAddFriendUIClass, "Mf", String.class, new XC_MethodHook() {
                 @Override
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                     super.beforeHookedMethod(param);
                     Log.d("FTSAddFriendUI","Mf--->"+param.args[0].toString());
+                }
+            });
+
+            //Hook FTSAddFriendUI$5.onSceneEnd
+            findAndHookMethod(FTSAddFriendUI$5Class, "onSceneEnd", int.class, int.class, String.class, mClass, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    super.beforeHookedMethod(param);
+                    Log.d("FTSAddFriendUI","r7--->"+param.args[0]+"->>>r8--->"+param.args[1]+"->>>r9--->"+param.args[2]+"->>>r10--->"+param.args[3].toString());
                 }
             });
 
@@ -51,13 +63,27 @@ public class crackmain implements IXposedHookLoadPackage{
                 @Override
                 protected void afterHookedMethod(final MethodHookParam param)throws Throwable{
                     Log.d("FTSAddFriendUI","Try Call Mf Method");
+                    Log.d("FTSAddFriendUI","--->"+XposedHelpers.findField(FTSAddFriendUIClass,"query").get(param.thisObject));
                     new Handler().postDelayed(new Runnable() {
                         @Override
                         public void run(){
                             Log.d("FTSAddFriendUI","重载run");
-                            callMethod(param.thisObject,"Mf","【嘻嘻】");
+                            XposedHelpers.setObjectField(param.thisObject,"query","{}");
+                            callMethod(param.thisObject,"Mf","{}");
                         }
                     },2000);
+                    Object obj=XposedHelpers.findClass("com.tencent.mm.pluginsdk.ui.applet.a", lpparam.classLoader).newInstance();
+                    Log.d("FTSAddFriendUI","GetDataClass--uzK=" + XposedHelpers.findField(XposedHelpers.findClass("com.tencent.mm.pluginsdk.ui.applet.a", lpparam.classLoader), "uzK").get(obj) +
+                            "--uzL=" + XposedHelpers.findField(XposedHelpers.findClass("com.tencent.mm.pluginsdk.ui.applet.a", lpparam.classLoader), "uzL").get(obj) +
+                            "--oXa=" + XposedHelpers.findField(XposedHelpers.findClass("com.tencent.mm.pluginsdk.ui.applet.a", lpparam.classLoader), "oXa").get(obj) +
+                            "--uzG=" + XposedHelpers.findField(XposedHelpers.findClass("com.tencent.mm.pluginsdk.ui.applet.a", lpparam.classLoader), "uzG").get(obj) +
+                            "--uzI=" + XposedHelpers.findField(XposedHelpers.findClass("com.tencent.mm.pluginsdk.ui.applet.a", lpparam.classLoader), "uzI").get(obj) +
+                            "--mdP=" + XposedHelpers.findField(XposedHelpers.findClass("com.tencent.mm.pluginsdk.ui.applet.a", lpparam.classLoader), "mdP").get(obj) +
+                            "--hPC=" + XposedHelpers.findField(XposedHelpers.findClass("com.tencent.mm.pluginsdk.ui.applet.a", lpparam.classLoader), "hPC").get(obj) +
+                            "--uzJ=" + XposedHelpers.findField(XposedHelpers.findClass("com.tencent.mm.pluginsdk.ui.applet.a", lpparam.classLoader), "uzJ").get(obj) +
+                            "--uzN=" + XposedHelpers.findField(XposedHelpers.findClass("com.tencent.mm.pluginsdk.ui.applet.a", lpparam.classLoader), "uzN").get(obj) +
+                            "--jyu=" + XposedHelpers.findField(XposedHelpers.findClass("com.tencent.mm.pluginsdk.ui.applet.a", lpparam.classLoader), "jyu").get(obj)
+                    );
                 }
             });
 
