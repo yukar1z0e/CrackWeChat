65a1dba288a9084ce8fefd82f380e81d3ec548b6
yukar1z0e <yakumozoe@outlook.com>
2019-08-06 17:49:33 +0800

    "WeChat Plugin V1.0"
    Signed-off-by: yukar1z0e <yakumozoe@outlook.com>

---
+++ crackwx/app/src/main/java/com/yukar1z0e/crackwx/MainActivity.java
@@ -0,0 +1,14 @@
+package com.yukar1z0e.crackwx;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
